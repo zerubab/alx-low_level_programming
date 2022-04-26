d74d40cb2787d6bf27129863533da5f79161b234
zerubab <yassienm863@gmail.com>
2022-04-26 14:27:44 +0100

    header

--- main.h
+++ main.h
@@ -9,25 +9,35 @@
 
 
 
-#include "main.h"
-/**
- * _memset - fills memory with a constant byte
- * @s: source string
- * @b: the constant byte for filling
- * @n: lenght of buffer
- * Return: new string
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int i = 0;
-while (i < n)
-{
-*(s + i) = b;
-i++;
-}
 
-return (s);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int coincidence(char *a, char *b);
+
+
+
+
 
 
 
